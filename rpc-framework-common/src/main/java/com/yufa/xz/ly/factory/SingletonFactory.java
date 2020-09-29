4dd9f40b388275e60206bf290e05e806dd3e52a9
YeLiu <1063042553@qq.com>
2020-09-29 14:11:44 +0800

    修改了单例工厂类的生成实例的方法
    * 将 get 方法放入同步代码块内，修复因为同步问题产生的bug。
    * 加入双重校验，在已有单例对象的情况下，避免进入同步代码块。

--- rpc-framework-common/src/main/java/com/yufa/xz/ly/factory/SingletonFactory.java
+++ rpc-framework-common/src/main/java/com/yufa/xz/ly/factory/SingletonFactory.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 /**
  * 获取单例对象的工厂类
+ *
  * @author LiuYe
  * @data 2020/9/16
  */
@@ -17,13 +18,16 @@ public class SingletonFactory {
     public static <T> T getInstance(Class<T> c) {
         String key = c.toString();
         Object instance = OBJECT_MAP.get(key);
-        synchronized (c){
-            if (instance == null){
-                try {
-                    instance = c.newInstance();
-                    OBJECT_MAP.put(key, instance);
-                } catch (IllegalAccessException | InstantiationException e) {
-                    throw new RuntimeException(e.getMessage(), e);
+        if (instance == null) {
+            synchronized (c) {
+                instance = OBJECT_MAP.get(key);
+                if (instance == null) {
+                    try {
+                        instance = c.newInstance();
+                        OBJECT_MAP.put(key, instance);
+                    } catch (IllegalAccessException | InstantiationException e) {
+                        throw new RuntimeException(e.getMessage(), e);
+                    }
                 }
             }
         }
